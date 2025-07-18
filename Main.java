9e3b5957bb28106e93dc9627045ca9da7a8dda64
Pranav Boyapati <pranav13b@gmail.com>
2025-07-17 22:26:32 -0500

    Add javadoc comments

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main 
 {
+    /**
+     * This method serves as the entry point for the application.
+     * 
+     * @param args command line arguments (not used)
+     */
     public static void main(String[] args)
     {
         CreateUI.initUI();
